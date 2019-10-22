76116186db29e95b87b7972cc0736900c832516b
kargo <karenrgo@gmail.com>
2019-10-22 16:16:22 -0700

    print pascal level with O(k) space

--- printPascalsTriangle.js
+++ printPascalsTriangle.js
@@ -3,7 +3,7 @@
 let printPascals = levels => {
   let triangle = []
 
-  for (let j = 0; j < levels; j++) {
+  for (let j = 0; j <= levels; j++) {
     let level = []
     for (let i = 0; i <= j; i++) {
       if (i === 0 || i === j) {
@@ -42,3 +42,20 @@ let kthPascals = k => {
 
 let kthresult = kthPascals(9)
 console.log(kthresult)
+
+// print the kth level of pascals triangle with O(k) space
+
+let kspacePascals = k => {
+  let output = []
+  for (let i = 0; i < k; i++) {
+    if (i === 0 || i === k) {
+      output.push(1)
+    } else {
+      output.push(output[i - 1] * (k - i + 1) / i)
+    }
+  }
+  return output
+}
+
+let kspace = kspacePascals(9)
+console.log(kspace)
